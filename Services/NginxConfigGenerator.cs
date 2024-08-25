a357ad19fe45f6c5f22ef0a9f354487061ede7e5
jeroenflvr <jeroen@flexworks.eu>
2024-08-25 14:25:00 +0200

    chore: cleanup comments

--- Services/NginxConfigGenerator.cs
+++ Services/NginxConfigGenerator.cs
@@ -3,17 +3,6 @@ using System.Text;
 using FlexSiteConfig.Models;
 
 
-// upstream backend_servers {
-//     server 192.168.0.2:3001 max_fails=3 fail_timeout=30s;
-// #    server 192.168.0.198:3001 max_fails=3 fail_timeout=30s;
-//     server 192.168.0.15:3002 max_fails=3 fail_timeout=30s;
-//     server 192.168.0.246:3005 max_fails=3 fail_timeout=30s;
-//     server 192.168.0.246:4001 max_fails=3 fail_timeout=30s;
-//     server 192.168.0.15:4000 max_fails=3 fail_timeout=30s;
-//     server 192.168.0.15:3003 max_fails=3 fail_timeout=30s;
-// }
-
-
 namespace FlexSiteConfig.Services {
     public static class NginxConfigGenerator {
         public static string GenerateConfig(Dictionary<string, Backend> backends) {
@@ -28,7 +17,7 @@ namespace FlexSiteConfig.Services {
                 sb.AppendLine($"upstream {backend.Key} {{");
                 foreach (var server in backend.Value.BackendServers)
                 {
-                    sb.AppendLine($"    server {server.ServerName}:{server.Port};");
+                    sb.AppendLine($"    server {server.ServerName}:{server.Port}  max_fails=3 fail_timeout=30;");
                     backendServerBlock.AppendLine($"    server {server.ServerName}:{server.Port} max_fails=3 fail_timeout=30s;");
                 }
                 sb.AppendLine("}");
